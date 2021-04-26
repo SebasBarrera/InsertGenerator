2f3106c5b78e418923dc228eea9d0cb7dce2873c
Sebastián Barrera <juan.barrera4@correo.icesi.edu.co>
2021-04-25 21:01:38 -0500

    first commit

---
+++ src/ui/Main.java
@@ -0,0 +1,21 @@
+package ui;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int tuplas =  sc.nextInt();
+		if (tuplas < 20) {
+			System.out.println("Minimo debe ingresar 20 tuplas");
+		} else {
+			for (int i = 0; i < tuplas; i++) {
+				
+			}
+		}
+
+	}
+
+}
